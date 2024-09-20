a75802c6ac0bd179db5fed95803f2e50d88cd265
Teju Thomas <thomasteju7@gmail.com>
2024-09-21 00:53:03 +0530

    Implement bottom tool bar, node db, api routes

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -7,6 +7,7 @@ import { Link, router } from "expo-router";
 import OAuth from "@/components/OAuth";
 import { useSignUp } from "@clerk/clerk-expo";
 import { ReactNativeModal } from "react-native-modal";
+import { fetchAPI } from "@/lib/fetch";
 
 const SignUp = () => {
   const { isLoaded, signUp, setActive } = useSignUp();
@@ -50,6 +51,14 @@ const SignUp = () => {
 
       if (completeSignUp.status === "complete") {
         // TODO: create a db user
+        await fetchAPI("/(api)/user", {
+          method: "POST",
+          body: JSON.stringify({
+            name: form.name,
+            email: form.email,
+            clerkId: completeSignUp.createdUserId,
+          }),
+        });
 
         await setActive({ session: completeSignUp.createdSessionId });
         setVerification({ ...verification, state: "success" });
